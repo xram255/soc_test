9e8766fe19d8961d6d6235bde9401e3d48dadbda
RoshanCS <roshan.cs790@gmail.com>
2020-10-17 14:06:25 +0530

    Addition
    client and server files added

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-import threading
+from time import sleep as delay
 import socket
 
 PORT = 5050
 #SERVER = "192.168.8.100"
-
 SERVER = socket.gethostbyname(socket.gethostname())
-
+ADDR = (SERVER, PORT)
 ip = socket.gethostbyname('www.google.com')
-print(PORT)
+
+print("wait...")
+delay(2)
+print(SERVER, "  ", socket.gethostname(), "  ", ip)
